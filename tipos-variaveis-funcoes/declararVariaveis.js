4f982cbabeef23c578d23b3f1f1f00c2bff154f3
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-08 12:35:37 -0300

    entendendo truthy/false e declarações de variaveis

---
+++ tipos-variaveis-funcoes/declararVariaveis.js
@@ -0,0 +1,17 @@
+
+
+
+//TESTANDO CONST
+
+// const forma = 'quadrado';
+// const altura = 5;
+// const comprimento = 7;
+// let area;
+
+// if(forma === 'quadrado'){
+//     area = altura * comprimento;
+// }
+// //para triangulo
+// else{
+//     area = altura * comprimento / 2;
+// }
