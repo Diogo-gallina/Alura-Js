f5a75a0de4c1c484a5aa7083847c485e24111ef6
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-10 09:50:17 -0300

    Finalizando desafio procurando aluno

---
+++ arrays/procurandoAluno.js
@@ -0,0 +1,21 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+const exibeNomeENota = (aluno) => {
+    if (listaDeAlunosEMedias[0].includes(aluno)){
+        console.log(`${aluno} está cadastrado`);
+
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+
+        const media = listaDeAlunosEMedias[1][indice];
+
+        console.log(`${aluno} tem a média: ${media}`);
+
+    } else{
+        console.log('Aluno não encontrado');
+    }
+}
+
+exibeNomeENota("Ana");
