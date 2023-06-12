b239977a055ebe874cf3497d240fa95c8846cfc3
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-12 06:27:56 -0300

    Adicionando lista de exercícios p/ treinar arrays

---
+++ arrays/exercicios.js
@@ -0,0 +1,46 @@
+// Nível iniciante:
+// a) Crie um array vazio chamado "numbers".
+// b) Adicione os números de 1 a 10 no array.
+// c) Imprima o array no console.
+
+// Nível iniciante:
+// a) Crie um array com os números pares de 1 a 20 chamado "evenNumbers".
+// b) Imprima o array no console.
+
+// Nível iniciante:
+// a) Crie um array com cinco nomes de frutas chamado "fruits".
+// b) Adicione mais duas frutas ao array.
+// c) Imprima o array no console.
+
+// Nível intermediário:
+// a) Crie um array com cinco números chamado "numbers".
+// b) Some todos os números do array e armazene o resultado em uma variável chamada "sum".
+// c) Imprima o valor da soma no console.
+
+// Nível intermediário:
+// a) Crie um array com dez números aleatórios chamado "randomNumbers".
+// b) Encontre o maior número do array e imprima-o no console.
+
+// Nível intermediário:
+// a) Crie um array com cinco nomes de países chamado "countries".
+// b) Ordene o array em ordem alfabética e imprima-o no console.
+
+// Nível intermediário:
+// a) Crie um array com dez números chamado "numbers".
+// b) Verifique se todos os números do array são pares.
+// c) Imprima "Todos os números são pares" ou "Existem números ímpares" no console.
+
+// Nível intermediário:
+// a) Crie um array com cinco nomes de cidades chamado "cities".
+// b) Crie um novo array chamado "capitalizedCities" que contenha os nomes das cidades com a primeira letra maiúscula.
+// c) Imprima o novo array no console.
+
+// Nível intermediário:
+// a) Crie um array com dez números aleatórios chamado "randomNumbers".
+// b) Crie um novo array chamado "doubledNumbers" que contenha cada número do array original multiplicado por 2.
+// c) Imprima o novo array no console.
+
+// Nível intermediário:
+// a) Crie um array com cinco nomes de animais chamado "animals".
+// b) Verifique se o array contém o animal "gato".
+// c) Imprima "O array contém o animal 'gato'" ou "O array não contém o animal 'gato'" no console.
