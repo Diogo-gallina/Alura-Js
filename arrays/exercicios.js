6a7fce2cc5f411fd4c66a314a55377c0a66b9cbd
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-13 06:53:04 -0300

    Finalizando ex maiornum do array

--- arrays/exercicios.js
+++ arrays/exercicios.js
@@ -12,18 +12,35 @@
 // b) Adicione mais duas frutas ao array.
 // c) Imprima o array no console.
 
+
+
+
+//------------------
 // Nível intermediário:
 // a) Crie um array com cinco números chamado "numbers".
 // b) Some todos os números do array e armazene o resultado em uma variável chamada "sum".
-// c) Imprima o valor da soma no console.
+// // c) Imprima o valor da soma no console.
+// const numbers = [10 , 15, 20, 5];
+// const somaArray = numbers.reduce((acc, somaArray) => somaArray + acc, 0);
+// console.log(somaArray);
+//-------------------
 
-const numbers = [10 , 15, 20, 5];
-const somaArray = numbers.reduce((acc, somaArray) => somaArray + acc, 0);
-console.log(somaArray);
+
 
 // Nível intermediário:
 // a) Crie um array com dez números aleatórios chamado "randomNumbers".
 // b) Encontre o maior número do array e imprima-o no console.
+
+const randomNumbers = [2, 5, 6, 4, 20, 45, 32, 12, 37, 29];
+let maior = randomNumbers[0]; 
+
+for (let i = 0; i < randomNumbers.length; i++) {
+  if (randomNumbers[i] > maior) {
+    maior = randomNumbers[i];
+  }
+}
+console.log(maior);
+
 
 // Nível intermediário:
 // a) Crie um array com cinco nomes de países chamado "countries".
@@ -35,14 +52,12 @@ const countries = ["Brasil", "Holanda", "México", "Arábia", "Australia"];
 
 
 
-
+//---------------------------------
 // Nível intermediário:
 // a) Crie um array com dez números chamado "numbers".
 // b) Verifique se todos os números do array são pares.
 // c) Imprima "Todos os números são pares" ou "Existem números ímpares" no console.
-
 // const numbers = [2, 2, 4, 6, 4, 2];
-
 // numbers.forEach(n => {
 //     result = false;
 //     if (n%2 == 0){
@@ -56,7 +71,7 @@ const countries = ["Brasil", "Holanda", "México", "Arábia", "Australia"];
 // } else{
 //     console.log("Contem numeros ímpares");
 // }
-
+//-----------------------------------
 
 
 
