2d657eadbd876f774876b028f6904e10811d9b0b
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-13 08:05:42 -0300

    declarando um objeto

---
+++ objetos/aula1/objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = [1111111, 222222222, 3333333];
+const informacoesPessoa = [ "nome", "Jose", "idade", 32, "CPF", "123145536"];
+
+console.log(informacoesPessoa [1]);
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32
+};
+
+console.log(objetoPessoa);
