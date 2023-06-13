7075079bfffa109c733a611486be64fbca315189
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-13 08:24:40 -0300

    alterando dados e propriedades de objetos

---
+++ objetos/aula1/alterar.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Luma",
+    profissao: "Engenheira"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 94645-3647";
+console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+console.log(pessoa);
