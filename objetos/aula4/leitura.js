e3e0597958c6f34dc9424d43927e4b1d0e1940d5
Diogo-Galllina <diogogallina20@gmail.com>
2023-06-15 06:50:05 -0300

    Operações de manipulação JSON

--- objetos/aula4/leitura.js
+++ objetos/aula4/leitura.js
@@ -1,4 +1,18 @@
+//Reqisitando dados Json
 const dados = require("./cliente.json");
 
 console.log(dados);
-console.log(typeof dados)
+console.log(typeof dados);
+
+//Transformando objeto em String
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+console.log(clienteEmString.nome);
+
+//Transformando String em objeto
+const objetoCliente = JSON.parse(clienteEmString);
+console.log(objetoCliente);
+console.log(typeof objetoCliente);
